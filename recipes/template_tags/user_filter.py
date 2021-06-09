fe792b5e694657d189b2e0e892bd37caf95a6960
Yauheni <smilodon2@yandex.ru>
2021-06-09 21:26:49 +0300

    добавил фильтр перевода секнд в минуты

---
+++ recipes/template_tags/user_filter.py
@@ -0,0 +1,12 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def duration(td):
+    total_seconds = int(td.total_seconds())
+    minutes = (total_seconds % 3600) // 60
+
+    return f'{minutes}  min.'
