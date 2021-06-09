f3962282bfb29f15909c445c47166bfb108d8b8b
Yauheni <smilodon2@yandex.ru>
2021-06-09 22:02:52 +0300

    добавил адрес страницы добавления рецепта

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import HomePage
+from .views import HomePage, AddRecipePage
 
 urlpatterns = [
     path('', HomePage.as_view(), name='index'),
+    path('add/', AddRecipePage.as_view(), name='add_recipe')
 ]
