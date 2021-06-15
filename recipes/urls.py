6dec83bfdef907a9b34bd685428eaaa5e2218194
Yauheni <smilodon2@yandex.ru>
2021-06-15 20:23:10 +0300

    добавил эндпойнт редактировния формы рецепта

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import HomePage, AddRecipePage #recipe_new
+from .views import HomePage, AddRecipePage, DetailRecipePage, EditRecipePage
 
 urlpatterns = [
     path('', HomePage.as_view(), name='index'),
-    path('add/', AddRecipePage.as_view(), name='add_recipe')
+    path('add/', AddRecipePage.as_view(), name='add_recipe'),
+    path('detail/<int:pk>/', DetailRecipePage.as_view(), name='detail_recipe'),
+    path('update/<int:pk>/', EditRecipePage.as_view(), name='update_recipe')
 ]
