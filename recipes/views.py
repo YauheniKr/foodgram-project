7919820cf7520bbc6e7dfd8112f9721e9800a09b
Yauheni <smilodon2@yandex.ru>
2021-06-16 20:25:19 +0300

    сделал пагинацию для домашней страницы сделал страницу для редактирования рецепта

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from .models import Recipe, Ingredient, Unit, Tag
 from .forms import RecipeForm
-from .utils import get_request_tags, save_recipe
-from django.contrib.auth.mixins import LoginRequiredMixin
+from .utils import get_request_tags, save_recipe, edit_recipe
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
+
+
+class MyPaginator(Paginator):
+    def validate_number(self, number):
+        try:
+            return super().validate_number(number)
+        except EmptyPage:
+            if int(number) > 1:
+                # return the last page
+                return self.num_pages
+            elif int(number) < 1:
+                # return the first page
+                return 1
+            else:
+                raise
 
 
 class HomePage(ListView):
     template_name = 'index.html'
     context_object_name = 'recipes'
-    paginate_by = 6
+    paginate_by = 4
+    paginator_class = MyPaginator
 
     def get_queryset(self):
         tags = get_request_tags(self.request)
@@ -20,10 +39,15 @@ class HomePage(ListView):
     def get_context_data(self, **kwargs):
         context = super(HomePage, self).get_context_data(**kwargs)
         context['all_tags'] = Tag.objects.all()
+        page = self.request.GET.get('page', 1)
+        recipes = HomePage.get_queryset(self)
+        paginator = self.paginator_class(recipes, self.paginate_by)
+        recipes = paginator.page(page)
+        context['recipes'] = recipes
         return context
 
 
-class AddRecipePage(CreateView, LoginRequiredMixin):
+class AddRecipePage(LoginRequiredMixin, CreateView):
     template_name = 'formRecipe.html'
     form_class = RecipeForm
 
@@ -38,9 +62,24 @@ class DetailRecipePage(DetailView):
     template_name = 'singlePage.html'
 
 
-class EditRecipePage(UpdateView):
+class EditRecipePage(LoginRequiredMixin, UpdateView):
     model = Recipe
-    template_name = 'singlePage.html'
+    template_name = 'formRecipe.html'
     context_object_name = 'recipe'
-    fields = ['title', ]
+    fields = ('title', 'image', 'text', 'cooking_time', 'tags',)
     pk_url_kwarg = 'pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(EditRecipePage, self).get_context_data(**kwargs)
+        context['all_tags'] = Tag.objects.all()
+        return context
+
+    def form_valid(self, form):
+        instance = Recipe.objects.get(id=self.kwargs["pk"])
+        edit_recipe(self.request, form, instance)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        args = self.kwargs["pk"]
+        success_url = reverse_lazy('detail_recipe', args=[args])
+        return success_url
