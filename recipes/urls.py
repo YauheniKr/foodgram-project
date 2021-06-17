706e1aed78a1b1a27bcb85dfde0b3f6699e00b2c
Yauheni <smilodon2@yandex.ru>
2021-06-17 22:06:06 +0300

    добавил точку для вывода списка рецептов на подписанных авторов

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from .views import HomePage, AddRecipePage, DetailRecipePage, EditRecipePage
+from .views import (HomePage, AddRecipePage, DetailRecipePage, EditRecipePage,
+                    ListFollowingPage,)
 
 urlpatterns = [
     path('', HomePage.as_view(), name='index'),
     path('add/', AddRecipePage.as_view(), name='add_recipe'),
     path('detail/<int:pk>/', DetailRecipePage.as_view(), name='detail_recipe'),
-    path('update/<int:pk>/', EditRecipePage.as_view(), name='update_recipe')
+    path('update/<int:pk>/', EditRecipePage.as_view(), name='update_recipe'),
+    path('follow/', ListFollowingPage.as_view(), name='follow'),
+
 ]
