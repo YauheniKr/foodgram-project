cca772adc525ad18eec99028c4c6299ac021c094
Yauheni <smilodon2@yandex.ru>
2021-06-17 22:04:13 +0300

    добавил энд пойнт для подписок

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientViewSet
+from .views import IngredientViewSet, SubscriptionViewSet
 
 router = DefaultRouter()
 router.register(r'ingredients', IngredientViewSet, 'ingredient_list')
+router.register(r'subscriptions', SubscriptionViewSet, 'subscriptions')
 
 urlpatterns = [
     path('', include(router.urls)),
