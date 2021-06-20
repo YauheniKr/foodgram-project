aa2d4f8417e93ec15fbf6e2b68b8e78021e8796f
Yauheni <smilodon2@yandex.ru>
2021-06-20 13:56:29 +0300

    добавил url Для страницы автора

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import (HomePage, AddRecipePage, DetailRecipePage, EditRecipePage,
-                    ListFollowingPage,)
+                    ListFollowingPage, ListRecipeAuthorPage)
 
 urlpatterns = [
     path('', HomePage.as_view(), name='index'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('detail/<int:pk>/', DetailRecipePage.as_view(), name='detail_recipe'),
     path('update/<int:pk>/', EditRecipePage.as_view(), name='update_recipe'),
     path('follow/', ListFollowingPage.as_view(), name='follow'),
+    path('author/<int:pk>/', ListRecipeAuthorPage.as_view(), name='author_page')
 
 ]
